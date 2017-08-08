8881ec563ef97dc40b646dddacca386fad8bff44
Jack Garrard <jg2562@nau.edu>
2017-08-08 12:45:32 -0700

    Added some vim keys to helm and remapped some global definitions to helm

--- configs/emacs.d/configs/init-helm.el
+++ configs/emacs.d/configs/init-helm.el
@@ -5,7 +5,16 @@
 (use-package helm
 		 :ensure t
 		 :config
-		 (helm-mode 1))
+		 (helm-mode 1)
+		 (define-key helm-map (kbd "C-j") 'helm-next-line)
+		 (define-key helm-map (kbd "C-k") 'helm-previous-line)
+		 (define-key helm-map (kbd "C-u") 'helm-previous-page)
+		 (define-key helm-map (kbd "C-d") 'helm-next-page)
+		 (define-key helm-find-files-map (kbd "C-h") 'helm-find-files-up-one-level)
+		 (define-key helm-find-files-map (kbd "C-l") 'helm-ff-RET)
+		 (define-key global-map (kbd "C-x C-f") 'helm-find-files)
+		 (define-key global-map (kbd "C-x C-b") 'helm-mini)
+		 (define-key global-map (kbd "M-x") 'helm-M-x))
 
 (provide 'init-helm)
 ;;; init-helm.el ends here
