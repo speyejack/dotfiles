7fd174894167845e433fd099759d93490fac4273
Jack Garrard <jg2562@nau.edu>
2017-08-08 13:30:29 -0700

    Added evil-org

--- configs/emacs.d/configs/init-org.el
+++ configs/emacs.d/configs/init-org.el
@@ -18,5 +18,15 @@ SCHEDULED: %t")))
 (define-key global-map (kbd "C-c c") 'my-org-task-capture)
 (setq org-blank-before-new-entry (quote ((heading) (plain-list-item))))
 (setq org-log-done (quote time))
+
+(use-package evil-org
+  :ensure t
+  :after org
+  :config
+  (add-hook 'org-mode-hook 'evil-org-mode)
+  (add-hook 'evil-org-mode-hook
+            (lambda ()
+              (evil-org-set-key-theme '(textobjects insert navigation additional shift todo heading)))))
+
 (provide 'init-org)
 ;;; init-org ends here
