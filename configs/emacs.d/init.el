3d38ec859e927adc03953b4c5650e1622eba0da7
Jack Garrard <jg2562@nau.edu>
2017-08-10 02:32:15 -0700

    Changed init to load config directories recursively

--- configs/emacs.d/init.el
+++ configs/emacs.d/init.el
@@ -5,7 +5,27 @@
 
 (package-initialize) ;; You might already have this line
 
-(add-to-list 'load-path (expand-file-name "configs" user-emacs-directory))
+
+(defun get-directories (dir)
+  "Return a list of all directories in a the given directory(DIR)."
+  (let ((dirs '()))
+	(let ((files (directory-files-and-attributes dir t)))
+	  (dolist (file files)
+		(let ((filename (car file)) (isdir (nth 1 file)))
+		  (when (and isdir (not (string-suffix-p "." filename)))
+			(push filename dirs))))
+	  (append dirs 'nil))))
+
+(defun recursively-load-directory (root)
+  "Recursively load every directory under root directory(ROOT)."
+  (let ((dirs (list root)))
+	(while dirs
+	  (let ((dir (pop dirs)))
+		(add-to-list 'load-path dir)
+		(setq dirs
+			  (append dirs (get-directories dir)))))))
+
+(recursively-load-directory (expand-file-name "configs" user-emacs-directory))
 
 (require 'init-elpa)
 (require 'user-config)
