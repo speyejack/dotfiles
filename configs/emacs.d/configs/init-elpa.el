6e2d53fe4ce950d464b673413973cc721b1876d7
Jack Garrard <jg2562@nau.edu>
2017-08-12 14:46:37 -0700

    Moved over basic initialization configs

---
+++ configs/emacs.d/configs/init-elpa.el
@@ -0,0 +1,14 @@
+;;; init-elpa --- Summary
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'package)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+(add-to-list 'package-archives '("elpy" . "https://jorgenschaefer.github.io/packages/"))
+
+(provide 'init-elpa)
+;;; init-elpa ends here
