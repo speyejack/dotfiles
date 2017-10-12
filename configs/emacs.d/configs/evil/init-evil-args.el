1b0b19b184d88bd8ab7c15b1053cb335f43e43f4
Jack Garrard <jg2562@nau.edu>
2017-10-11 22:57:50 -0700

    Enabled evil args

--- configs/emacs.d/configs/evil/init-evil-args.el
+++ configs/emacs.d/configs/evil/init-evil-args.el
@@ -4,7 +4,10 @@
 ;;; Code:
 
 (defun jag--set-evil-args-key-bindings ()
-  "Set up keybindings for evil-args.")
+  "Set up keybindings for evil-args."
+  ;; bind evil-args text objects
+  (define-key evil-inner-text-objects-map "a" 'evil-inner-arg)
+  (define-key evil-outer-text-objects-map "A" 'evil-outer-arg))
 
 (defun jag--setup-evil-args-config ()
   "Set up personal configuation for evil-args.")
