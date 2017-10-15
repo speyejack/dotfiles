3196e8ca4f80b07c220b08adb0f29be163b253e2
Jack Garrard <jg2562@nau.edu>
2017-10-14 21:59:15 -0700

    Reordered config layers

--- configs/emacs.d/configs/init-configs-layer.el
+++ configs/emacs.d/configs/init-configs-layer.el
@@ -5,19 +5,20 @@
 
 (defun jag--load-configs-layer-requires ()
   "Load each package for directory."
+  (require 'init-evil-layer)
+  (require 'init-helm-layer)
   (require 'init-company-layer)
   (require 'init-correction-layer)
-  (require 'init-evil-layer)
   (require 'init-fun-layer)
   (require 'init-git-layer)
-  (require 'init-helm-layer)
   (require 'init-languages-layer)
   (require 'init-org-layer)
   (require 'init-text-layer)
   (require 'init-utility-layer)
   (require 'init-visual-layer)
   (require 'init-emacs-layer)
-  (require 'init-web-layer))
+  (require 'init-web-layer)
+  )
 
 (jag--load-configs-layer-requires)
 
