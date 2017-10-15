97e0f7074195cc2a24f6f7f4df8d56e6e67fc277
Jack Garrard <jg2562@nau.edu>
2017-10-14 17:29:12 -0700

    Changed when company hooks are added

--- configs/emacs.d/configs/helm/init-helm-company.el
+++ configs/emacs.d/configs/helm/init-helm-company.el
@@ -10,8 +10,11 @@
 
 (defun jag--add-helm-company-key-hooks ()
   "Add keyboard hooks to helm-company."
-  (define-key company-mode-map (kbd "C-'") 'helm-company)
-  (define-key company-active-map (kbd "C-'") 'helm-company))
+  (add-hook 'company-mode-hook
+			(lambda ()
+			  (progn
+				(define-key company-mode-map (kbd "C-'") 'helm-company)
+				(define-key company-active-map (kbd "C-'") 'helm-company)))))
 
 (defun jag--add-helm-company-other-hooks ()
   "Add other hooks to helm-company.")
