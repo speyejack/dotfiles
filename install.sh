67b8e74c3324fb0744a5ebb860d419bd49732d96
Jack Garrard <speyejack@gmail.com>
2018-06-24 17:28:16 -0700

    Added an install script that runs the other install scripts

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+source ./configs/emacs.d/install.sh
+source ./configs/i3/install.sh
+source ./configs/i3blocks/install.sh
+source ./configs/zsh/install.sh
+source ./configs/ssh/install.sh
