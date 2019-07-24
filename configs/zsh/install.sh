6b600118b7644c6334a7cba4fa69140f8660bad4
Jack Garrard <jg2562@nau.edu>
2019-07-24 16:17:13 -0700

    Disabled oh my zsh and added ability to replace with my config

--- configs/zsh/install.sh
+++ configs/zsh/install.sh
@@ -4,7 +4,8 @@ SCRIPT=$(readlink -f "$0")
 # Absolute path this script is in, thus /home/user/bin
 SCRIPTPATH=$(dirname "$SCRIPT")
 
-ln -s -r $SCRIPTPATH/zshrc ~/.zshrc
-
 sudo apt-get install zsh curl git
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+rm -rf ~/.zshrc
+ln -s -r $SCRIPTPATH/zshrc ~/.zshrc
