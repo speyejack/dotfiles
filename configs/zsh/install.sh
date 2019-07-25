83f245694fd2b2cfa4cc8210600e671ef6dd0c31
jack <jack@siccs112-03.egr.nau.edu>
2019-07-24 19:21:26 -0700

    Readded oh my zsh

--- configs/zsh/install.sh
+++ configs/zsh/install.sh
@@ -5,7 +5,7 @@ SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
 sudo apt-get install zsh curl git
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 sudo chsh -s /usr/bin/zsh
 rm -rf ~/.zshrc
