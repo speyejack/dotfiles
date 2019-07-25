e4e8b73eea8767521871d43a5b5eae928bad0b54
Jack Garrard <jg2562@nau.edu>
2019-07-25 00:35:47 -0700

    Added termite install file

---
+++ configs/termite/install.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+
+sudo apt update
+sudo apt install build-essential
+
+sudo apt-get install -y git g++ libgtk-3-dev gtk-doc-tools gnutls-bin valac intltool libpcre2-dev libglib3.0-cil-dev libgnutls28-dev libgirepository1.0-dev libxml2-utils gperf
+
+# Install vte-ng
+cd ~/
+git clone https://github.com/thestinger/vte-ng.git
+echo export LIBRARY_PATH="/usr/include/gtk-3.0:$LIBRARY_PATH"
+cd vte-ng
+./autogen.sh
+make && sudo make install
+cd ..
+rm -rf vte-ng
+
+# Install termite
+cd ~/
+git clone --recursive https://github.com/thestinger/termite.git
+cd termite
+make
+sudo make install
+sudo ldconfig
+sudo mkdir -p /lib/terminfo/x
+sudo ln -s /usr/local/share/terminfo/x/xterm-termite /lib/terminfo/x/xterm-termite
+sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/local/bin/termite 60
+cd ..
+rm -rf termite
+
+ln -s -r $SCRIPTPATH ~/.config/
