df524370880ddfa5738fcf62e7a37cf9b059d52a
Jack Garrard <jg2562@nau.edu>
2019-08-05 16:42:53 -0700

    Fixed the paths being correct for general install script

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ SCRIPT=$(readlink -f "$0")
 # Absolute path this script is in, thus /home/user/bin
 SCRIPTPATH=$(dirname "$SCRIPT")
 
-source $SCRIPTPATH/configs/emacs.d/install.sh
-source $SCRIPTPATH/configs/i3/install.sh
-source $SCRIPTPATH/configs/i3blocks/install.sh
-source $SCRIPTPATH/configs/zsh/install.sh
-source $SCRIPTPATH/configs/ssh/install.sh
+$SCRIPTPATH/configs/emacs.d/install.sh
+$SCRIPTPATH/configs/i3/install.sh
+$SCRIPTPATH/configs/i3blocks/install.sh
+$SCRIPTPATH/configs/zsh/install.sh
+$SCRIPTPATH/configs/ssh/install.sh
