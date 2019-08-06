5071d37fcbdab2da6289b1b3e4a83678985e593c
Jack Garrard <jg2562@nau.edu>
2019-08-05 17:22:24 -0700

    Added termite install

--- install.sh
+++ install.sh
@@ -9,3 +9,4 @@ $SCRIPTPATH/configs/i3/install.sh
 $SCRIPTPATH/configs/i3blocks/install.sh
 $SCRIPTPATH/configs/zsh/install.sh
 $SCRIPTPATH/configs/ssh/install.sh
+$SCRIPTPATH/configs/termite/install.sh
