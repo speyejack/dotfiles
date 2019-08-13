d849849240397822b078b7c01f352f89fbcde337
jack <speyejack@gmail.com>
2019-08-13 16:41:10 -0700

    Updated the i3 install script to group up installs

--- configs/i3/install.sh
+++ configs/i3/install.sh
@@ -4,12 +4,12 @@ SCRIPT=$(readlink -f "$0")
 # Absolute path this script is in, thus /home/user/bin
 SCRIPTPATH=$(dirname "$SCRIPT")
 
-sudo apt install i3
-sudo apt install i3status
-sudo apt install i3blocks
-sudo apt install unclutter
+sudo apt install i3 i3status unclutter playerctl
 
+# Make caps lock the mod key
 echo -e "clear Lock\nkeycode 66 = Hyper_L\nadd mod4 = Hyper_L" >> ~/.Xmodmap
+
 touch $SCRIPTPATH/i3startup
 chmod +x $SCRIPTPATH/i3startup
+
 ln -s -r $SCRIPTPATH ~/.config/
