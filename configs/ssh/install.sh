24bfb1ae814f6171c9b56a5368d34f0090b25dba
jack <speyejack@gmail.com>
2019-08-13 16:33:35 -0700

    Improved the ssh install script to not wipe ssh dir

--- configs/ssh/install.sh
+++ configs/ssh/install.sh
@@ -3,4 +3,6 @@
 SCRIPT=$(readlink -f "$0")
 # Absolute path this script is in, thus /home/user/bin
 SCRIPTPATH=$(dirname "$SCRIPT")
+cp ~/.ssh/* $SCRIPTPATH
+rm -r ~/.ssh
 ln -s -r $SCRIPTPATH ~/.ssh
