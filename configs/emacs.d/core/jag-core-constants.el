a17bb9ef1e1d36d403483f602376fefc6694d0c5
jack <speyejack@gmail.com>
2019-08-16 16:11:03 -0700

    Changed the constants file to use the constant defined for loading

--- configs/emacs.d/core/jag-core-constants.el
+++ configs/emacs.d/core/jag-core-constants.el
@@ -10,25 +10,26 @@
   "When Emacs started evaling our code.
 Similar to `before-init-time'")
 
+
 (defconst jag-core-dir (expand-file-name "core" jag-dir)
   "The home of the core functionality.")
-(add-to-list 'load-path (expand-file-name "core" jag-dir))
+(add-to-list 'load-path jag-core-dir)
 
 (defconst jag-funcs-dir (expand-file-name "funcs" jag-dir)
   "The directory of custom functions to assist with maps and packages.")
-(add-to-list 'load-path (expand-file-name "funcs"))
+(add-to-list 'load-path jag-funcs-dir)
 
 (defconst jag-locals-dir (expand-file-name "locals" jag-dir)
   "The directory for local packages.")
-(add-to-list 'load-path (expand-file-name "locals"))
+(add-to-list 'load-path jag-locals-dir)
 
 (defconst jag-packages-dir (expand-file-name "packages" jag-dir)
   "The directory for downloaded packages.")
-(add-to-list 'load-path (expand-file-name "packages"))
+(add-to-list 'load-path jag-packages-dir)
 
 (defconst jag-modes-dir (expand-file-name "modes" jag-dir)
   "The directory for mode specific packages.")
-(add-to-list 'load-path (expand-file-name "modes"))
+(add-to-list 'load-path jag-modes-dir)
 
 
 (provide 'jag-core-constants)
