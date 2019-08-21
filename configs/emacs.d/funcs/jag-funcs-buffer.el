3af96972bcb45ed7f3b1cd703cee867892e4f47f
jack <speyejack@gmail.com>
2019-08-20 19:03:45 -0700

    Added a force kill buffer key

--- configs/emacs.d/funcs/jag-funcs-buffer.el
+++ configs/emacs.d/funcs/jag-funcs-buffer.el
@@ -5,5 +5,14 @@
 
 ;;; Code:
 
+(defun jag-force-kill-this-buffer (&optional arg)
+  "Force kill this buffer.
+If the universal prefix argument is used then kill the window too."
+  (interactive "P")
+  (let ((kill-buffer-query-functions (delq 'process-kill-buffer-query-function kill-buffer-query-functions)))
+	(kill-this-buffer)
+	(if (equal '(4) arg)
+		(delete-window))))
+
 (provide 'jag-funcs-buffer)
 ;;; jag-funcs-buffer.el ends here
