61b9857e7dddce18d8b42af8b791eb349701a873
jack <speyejack@gmail.com>
2019-08-23 14:14:37 -0700

    Added yank key to keys

---
+++ configs/emacs.d/keys/jag-keys-yank.el
@@ -0,0 +1,24 @@
+;;; jag-keys-yank.el --- Config for key yank
+
+;;; Commentary:
+
+;;; Code:
+
+;; Create the yank leader definer
+(general-create-definer jag--yank-leader-def
+  :which-key "yank leader prefix"
+  :states '(normal visual operator)
+  :prefix (concat jag-leader-key " y")
+  :global-prefix (concat jag-emacs-leader-key " y"))
+
+(use-package jag-funcs-yank
+  :ensure nil
+  :general
+  (jag--yank-leader-def
+   "d" 'jag-yank-directory-path
+   "f" 'jag-yank-file-path
+   "m" 'jag-yank-last-message
+   "b" 'jag-copy-whole-buffer-to-clipboard))
+
+(provide 'jag-keys-yank)
+;;; jag-keys-yank.el ends here
