e070e6111721c818b5f68fa0d6faef6c04b07908
speyejack <speyejack@gmail.com>
2019-09-05 18:56:05 +0000

    Fixed project leader search

--- configs/emacs.d/keys/jag-keys-project.el
+++ configs/emacs.d/keys/jag-keys-project.el
@@ -13,30 +13,32 @@
   :prefix (concat jag-leader-key " p")
   :global-prefix (concat jag-emacs-leader-key " p"))
 
+(use-package jag-funcs-project
+  :commands 'jag-helm-projectile-search
+  :ensure nil)
 
 (jag--project-leader-def
- "SPC" 'helm-projectile
- "!" 'projectile-run-shell-command-in-root
- "%" 'projectile-replace-regexp
- "&" 'projectile-run-async-shell-command-in-root
- "D" 'projectile-dired
- "F" 'helm-projectile-find-file-dwim
- "G" 'projectile-regenerate-tags
- "I" 'projectile-invalidate-cache
- "R" 'projectile-replace
- "T" 'projectile-test-project
- "a" 'projectile-toggle-between-implementation-and-test
- "b" 'helm-projectile-switch-to-buffer
- "c" 'projectile-compile-project
- "d" 'helm-projectile-find-dir
- "f" 'helm-projectile-find-file
- "g" 'projectile-find-tag
- "k" 'projectile-kill-buffers
- "p" 'projectile-switch-project
- "r" 'helm-projectile-recentf
- "s" 'jag-helm-projectile
- "v" 'projectile-vc)
-
+  "SPC" 'helm-projectile
+  "!" 'projectile-run-shell-command-in-root
+  "%" 'projectile-replace-regexp
+  "&" 'projectile-run-async-shell-command-in-root
+  "D" 'projectile-dired
+  "F" 'helm-projectile-find-file-dwim
+  "G" 'projectile-regenerate-tags
+  "I" 'projectile-invalidate-cache
+  "R" 'projectile-replace
+  "T" 'projectile-test-project
+  "a" 'projectile-toggle-between-implementation-and-test
+  "b" 'helm-projectile-switch-to-buffer
+  "c" 'projectile-compile-project
+  "d" 'helm-projectile-find-dir
+  "f" 'helm-projectile-find-file
+  "g" 'projectile-find-tag
+  "k" 'projectile-kill-buffers
+  "p" 'projectile-switch-project
+  "r" 'helm-projectile-recentf
+  "s" 'jag-helm-projectile-search
+  "v" 'projectile-vc)
 
 (provide 'jag-keys-project)
 
