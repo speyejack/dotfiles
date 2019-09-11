914ce717b2751f8b57f7362929d72f9a498058e8
speyejack <speyejack@gmail.com>
2019-09-10 19:38:59 -0700

    Fixed yanking buffer not working

--- configs/emacs.d/keys/jag-keys-yank.el
+++ configs/emacs.d/keys/jag-keys-yank.el
@@ -11,14 +11,19 @@
   :prefix (concat jag-leader-key " y")
   :global-prefix (concat jag-emacs-leader-key " y"))
 
+(use-package jag-funcs-buffer
+  :ensure nil
+  :general
+  (jag--yank-leader-def
+  "b" 'jag-copy-whole-buffer-to-clipboard))
+
 (use-package jag-funcs-yank
   :ensure nil
   :general
   (jag--yank-leader-def
    "d" 'jag-yank-directory-path
    "f" 'jag-yank-file-path
-   "m" 'jag-yank-last-message
-   "b" 'jag-copy-whole-buffer-to-clipboard))
+   "m" 'jag-yank-last-message))
 
 (provide 'jag-keys-yank)
 ;;; jag-keys-yank.el ends here
