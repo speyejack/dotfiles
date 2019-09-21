6afd03b32bba7e6626bf803c8bbdd277de6d0222
speyejack <speyejack@gmail.com>
2019-09-20 21:12:45 -0700

    Added helm google search for duck duck go

--- configs/emacs.d/keys/jag-keys-web.el
+++ configs/emacs.d/keys/jag-keys-web.el
@@ -13,6 +13,9 @@
   :prefix (concat jag-leader-key " l")
   :global-prefix (concat jag-emacs-leader-key " l"))
 
+;; Should add duck duck go as suggestion url
+(setq helm-google-suggest-search-url "https://duckduckgo.com/?q=%s&t=canonical&atb=v185-2&ia=web")
+
 (jag--web-leader-def
  "g" 'engine/search-github
  "S" 'engine/search-stack-overflow
@@ -21,7 +24,7 @@
  "H" 'howdoi-query-line-at-point
  "w" 'engine/search-wikipedia
  "d" 'engine/search-duckduckgo
- "l" 'engine/search-duckduckgo
+ "l" 'helm-google-suggest
  "a" 'engine/search-amazon
  "x" 'xkcd
  "y" 'engine/search-youtube)
