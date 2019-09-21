6e7ef70d8111f5b854f5d09dbdc298901f3a34e5
speyejack <speyejack@gmail.com>
2019-09-20 21:11:49 -0700

    Unbound local mode key to prevent conflicts

--- configs/emacs.d/keys/jag-keys-local.el
+++ configs/emacs.d/keys/jag-keys-local.el
@@ -6,6 +6,10 @@
 
 ;;; Code:
 
+(general-unbind
+  :states '(motion)
+  "SPC m")
+
 ;; Create the local leader definer
 (general-create-definer jag--local-leader-def
   :global-prefix (concat jag-emacs-leader-key " m")
