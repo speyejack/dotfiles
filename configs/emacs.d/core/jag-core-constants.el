8396bcadd8258d365c76d571b087ab0259f41444
speyejack <speyejack@gmail.com>
2019-11-16 00:16:55 -0700

    Added a macro to create constant dirs

--- configs/emacs.d/core/jag-core-constants.el
+++ configs/emacs.d/core/jag-core-constants.el
@@ -10,31 +10,19 @@
   "When Emacs started evaling our code.
 Similar to `before-init-time'")
 
+(defmacro jag--def-and-load-dir (def name desc)
+  "Create a constant DEF for directory NAME with description DESC and then load it."
+  `(progn
+	 (defconst ,def (expand-file-name ,name jag-dir)
+	   ,desc)
+	 (add-to-list 'load-path ,def)))
 
-(defconst jag-core-dir (expand-file-name "core" jag-dir)
-  "The home of the core functionality.")
-(add-to-list 'load-path jag-core-dir)
-
-(defconst jag-keys-dir (expand-file-name "keys" jag-dir)
-  "The directory for key bindings.")
-(add-to-list 'load-path jag-keys-dir)
-
-(defconst jag-funcs-dir (expand-file-name "funcs" jag-dir)
-  "The directory of custom functions to assist with maps and packages.")
-(add-to-list 'load-path jag-funcs-dir)
-
-(defconst jag-locals-dir (expand-file-name "locals" jag-dir)
-  "The directory for local packages.")
-(add-to-list 'load-path jag-locals-dir)
-
-(defconst jag-modules-dir (expand-file-name "modules" jag-dir)
-  "The directory for packages grouped into modules.")
-(add-to-list 'load-path jag-modules-dir)
-
-(defconst jag-modes-dir (expand-file-name "modes" jag-dir)
-  "The directory for mode specific packages.")
-(add-to-list 'load-path jag-modes-dir)
-
+(jag--def-and-load-dir jag-core-dir "core" "The home of the core functionality.")
+(jag--def-and-load-dir jag-keys-dir "keys" "The directory for key bindings.")
+(jag--def-and-load-dir jag-funcs-dir "funcs" "The directory of custom functions to assist with maps and packages.")
+(jag--def-and-load-dir jag-locals-dir "locals" "The directory for local packages.")
+(jag--def-and-load-dir jag-modules-dir "modules" "The directory for packages grouped into modules.")
+(jag--def-and-load-dir jag-modes-dir "modes" "The directory for mode specific packages.")
 
 
 (defconst jag-notes-dir (expand-file-name "notes" jag-dir)
