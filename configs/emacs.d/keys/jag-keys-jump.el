03f1bdc1757ef945b0e10a68291429322c03952d
speyejack <speyejack@gmail.com>
2019-11-23 16:33:44 -0700

    Made jump keys load associated funcs package

--- configs/emacs.d/keys/jag-keys-jump.el
+++ configs/emacs.d/keys/jag-keys-jump.el
@@ -11,23 +11,28 @@
 (jag-declare-prefixes
  '(("j" "jump")))
 
-(jag-define-keys jag-jump-map
-  "b" 'ibuffer-jump
-  "d" 'dired-jump
-  "D" 'dired-jump-other-window
-  "J" 'evil-avy-goto-char-2
-  "j" 'evil-avy-goto-char
-  "l" 'evil-avy-goto-line
-  "w" 'evil-avy-goto-word-or-subword-1
-  "u" 'jag-avy-open-url
-  "U" 'jag-avy-goto-url
-  "o" 'open-line
-  "f" 'helm-imenu-in-all-buffers
-  "F" 'helm-semantic-or-imenu
-  "k" 'dumb-jump-go
-  "K" 'dumb-jump-back
-  "G" 'evil-window-bottom
-  "g" 'evil-window-top)
+(use-package jag-funcs-jump
+  :ensure nil
+  :bind
+  (:map jag-jump-map
+  ("b" . 'ibuffer-jump)
+  ("d" . 'dired-jump)
+  ("D" . 'dired-jump-other-window)
+  ("J" . 'evil-avy-goto-char-2)
+  ("j" . 'evil-avy-goto-char)
+  ("l" . 'evil-avy-goto-line)
+  ("w" . 'evil-avy-goto-word-or-subword-1)
+  ("u" . 'jag-avy-open-url)
+  ("U" . 'jag-avy-goto-url)
+  ("o" . 'open-line)
+  ("f" . 'helm-imenu-in-all-buffers)
+  ("F" . 'helm-semantic-or-imenu)
+  ("k" . 'dumb-jump-go)
+  ("K" . 'dumb-jump-back)
+  ("G" . 'evil-window-bottom)
+  ("g" . 'evil-window-top)))
+
+
 
 
 (provide 'jag-keys-jump)
