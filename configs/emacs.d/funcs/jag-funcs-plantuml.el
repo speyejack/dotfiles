8ffebc1c059674caf2ad7477a64f02b18f2264ef
speyejack <speyejack@gmail.com>
2019-11-23 22:14:58 -0700

    Added plantuml major mode

---
+++ configs/emacs.d/funcs/jag-funcs-plantuml.el
@@ -0,0 +1,10 @@
+;;; jag-funcs-plantuml.el --- Functions for plantuml
+
+;;; Commentary:
+
+;;; Code:
+
+
+
+(provide 'jag-funcs-plantuml)
+;;; jag-funcs-plantuml.el ends here
---
+++ configs/emacs.d/modes/jag-modes-plantuml.el
@@ -0,0 +1,13 @@
+;;; jag-modes-plantuml.el --- Config for mode plantuml
+
+;;; Commentary:
+
+;;; Code:
+
+(use-package jag-funcs-plantuml
+  :ensure nil)
+
+(use-package plantuml)
+
+(provide 'jag-modes-plantuml)
+;;; jag-modes-plantuml.el ends here
