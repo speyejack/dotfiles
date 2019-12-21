dab6946e7494947bd9dd397c42ca347e4fbefe96
speyejack <speyejack@gmail.com>
2019-12-20 19:27:07 -0700

    Removed last garbage collection as gcmh will handle it

--- configs/emacs.d/start.el
+++ configs/emacs.d/start.el
@@ -78,10 +78,6 @@
 (require 'jag-modes-xml)
 (require 'jag-modes-yaml)
 
-
-;; Make gc pauses faster by decreasing the threshold.
-(setq gc-cons-threshold (* 2 1000 1000))
-
 ;; Use a hook so the message doesn't get clobbered by other messages.
 (add-hook 'emacs-startup-hook
 		  (lambda ()
