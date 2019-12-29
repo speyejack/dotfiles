a52bfe5fc77d439597ea78111656d92ffef9ae16
speyejack <speyejack@gmail.com>
2019-12-28 22:23:07 -0700

    Changed def-and-load-dir macro to define symbol

--- configs/emacs.d/core/jag-core-constants.el
+++ configs/emacs.d/core/jag-core-constants.el
@@ -10,19 +10,20 @@
   "When Emacs started evaling our code.
 Similar to `before-init-time'")
 
-(defmacro jag--def-and-load-dir (def name desc)
-  "Create a constant DEF for directory NAME with description DESC and then load it."
+(defmacro jag--def-and-load-dir (name desc)
+  "Create a constant for directory NAME with description DESC and then load it."
+  (let ((dir-symbol (intern (format "jag-%s-dir" name))))
   `(progn
-	 (defconst ,def (expand-file-name ,name jag-dir)
+	 (defconst ,dir-symbol (expand-file-name ,name jag-dir)
 	   ,desc)
-	 (add-to-list 'load-path ,def)))
+	 (add-to-list 'load-path ,dir-symbol))))
 
-(jag--def-and-load-dir jag-core-dir "core" "The home of the core functionality.")
-(jag--def-and-load-dir jag-keys-dir "keys" "The directory for key bindings.")
-(jag--def-and-load-dir jag-funcs-dir "funcs" "The directory of custom functions to assist with maps and packages.")
-(jag--def-and-load-dir jag-locals-dir "locals" "The directory for local packages.")
-(jag--def-and-load-dir jag-modules-dir "modules" "The directory for packages grouped into modules.")
-(jag--def-and-load-dir jag-modes-dir "modes" "The directory for mode specific packages.")
+(jag--def-and-load-dir "core" "The home of the core functionality.")
+(jag--def-and-load-dir "keys" "The directory for key bindings.")
+(jag--def-and-load-dir "funcs" "The directory of custom functions to assist with maps and packages.")
+(jag--def-and-load-dir "locals" "The directory for local packages.")
+(jag--def-and-load-dir "modules" "The directory for packages grouped into modules.")
+(jag--def-and-load-dir "modes" "The directory for mode specific packages.")
 
 
 (defconst jag-notes-dir (expand-file-name "notes" jag-dir)
