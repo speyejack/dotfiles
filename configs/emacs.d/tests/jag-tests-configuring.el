a2e7840af54b9ffdcf62aab1f70a41634a49353c
speyejack <speyejack@gmail.com>
2020-01-06 00:07:12 -0700

    Created a test for new template name

--- configs/emacs.d/tests/jag-tests-configuring.el
+++ configs/emacs.d/tests/jag-tests-configuring.el
@@ -6,5 +6,12 @@
 
 (require 'jag-funcs-configuring)
 
+(ert-deftest jag-tests-configuring--new-template-name ()
+  "Tests the generation of new template names."
+  (should (string-equal (jag--new-template-name "test" "template") "jag-test-template"))
+  (should (string-equal (jag--new-template-name "test" "") "jag-test-"))
+  (should (string-equal (jag--new-template-name "" "template") "jag--template"))
+  (should (string-equal (jag--new-template-name "funcs" "testing") "jag-funcs-testing")))
+
 (provide 'jag-tests-configuring)
 ;;; jag-tests-configuring.el ends here
