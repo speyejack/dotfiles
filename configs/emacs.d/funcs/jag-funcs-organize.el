e09387b8bda6aee0ea4fee0c65e6c21bedfe8a70
jack <speyejack@gmail.com>
2020-01-29 12:36:13 -0700

    Added better interactive calls to custom capture functions

--- configs/emacs.d/funcs/jag-funcs-organize.el
+++ configs/emacs.d/funcs/jag-funcs-organize.el
@@ -5,28 +5,28 @@
 ;;; Code:
 
 
-(defun jag--quick-org-task-capture ()
+(defun jag--quick-org-task-capture (&optional goto)
   "Capture a task with my default template."
-  (interactive)
-  (org-capture nil "t"))
+  (interactive "P")
+  (org-capture goto "t"))
 
-(defun jag-org-file-task-capture ()
+(defun jag-org-file-task-capture (&optional goto)
   "Capture a task with a file or project."
-  (interactive)
+  (interactive "P")
   (if (projectile-project-p)
 	  (org-projectile-capture-for-current-project)
-	(org-capture nil "h")))
+	(org-capture goto "h")))
 
-(defun jag-org-clock-in-capture ()
+(defun jag-org-clock-in-capture (&optional goto)
   "Capture the name of a clock in and start."
-  (interactive)
+  (interactive "P")
   (org-resolve-clocks)
-  (org-capture nil "c"))
+  (org-capture goto "c"))
 
-(defun jag-org-due-capture ()
+(defun jag-org-due-capture (&optional goto)
   "Capture a due date."
-  (interactive)
-  (org-capture nil "d"))
+  (interactive "P")
+  (org-capture goto "d"))
 
 (provide 'jag-funcs-organize)
 ;;; jag-funcs-organize.el ends here
