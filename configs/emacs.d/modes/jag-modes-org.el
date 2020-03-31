c13d75dc3104767fc35fa484a594e61657d0f3ab
speyejack <speyejack@gmail.com>
2020-03-31 14:19:48 -0700

    Changed gtd refile files to single level as that better fits my style

--- configs/emacs.d/modes/jag-modes-org.el
+++ configs/emacs.d/modes/jag-modes-org.el
@@ -214,9 +214,9 @@
 
 
 	(setq org-refile-targets
-		  `((,gtd-file :maxlevel . 2)
-			(,tickler-file :maxlevel . 2)
-			(,someday-file :maxlevel . 1)))
+		  `((,gtd-file :level . 2)
+			(,tickler-file :level . 2)
+			(,someday-file :level . 1)))
 
 	(setq org-agenda-files
 		  `(,inbox-file ,gtd-file ,tickler-file))
