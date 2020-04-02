f670d6ea1365c0310a2c1565f56661ea1da1d49f
speyejack <speyejack@gmail.com>
2020-04-01 18:53:40 -0700

    Reorganized organize keymap to be organized into groups

--- configs/emacs.d/keys/jag-keys-organize.el
+++ configs/emacs.d/keys/jag-keys-organize.el
@@ -8,36 +8,63 @@
 
 (jag-declare-prefixes
  '(("o" "organize")
-   ("og" "goto")))
+   ("og" "goto")
+   ("oc" "clock")
+   ("or" "capture")
+   ("oR" "remember")
+   ("oK" "roam")))
 
 (jag-define-keys jag-organize-map
  "a" 'org-agenda
- "d" 'jag-org-due-capture
+
+ "h" 'calendar
+
  "o" 'jag--quick-org-task-capture
  "O" 'jag-org-file-task-capture
- "b" 'evil-operator-remember
- "B" 'remember
- "R" 'org-roam
- "h" 'calendar
+ "d" 'jag-org-due-capture
  "i" 'jag-org-interrupt-capture
+ "s" 'jag-org-schedule-capture
+
  "t" 'jag-org-clock-in-capture
  "T" 'jag-org-clock-gtd
- "u" 'org-clock-in-last
  "y" 'org-clock-out
  "Y" 'org-clock-cancel
+
+ "cd" 'org-clock-display
+ "ce" 'org-clock-modify-effort-estimate
+ "cf" 'org-resolve-clocks
+ "cr" 'org-clock-report
+ "ct" 'org-clock-in
+ "cu" 'org-clock-in-last
+ "cy" 'org-clock-out
+ "cc" 'jag-org-clock-select-task
+
  "j" 'jag-org-journal-capture
  "J" 'org-journal-new-entry
+
  "k" 'org-roam-find-file
- "K" 'org-roam-today
+ "Kk" 'org-roam
+ "Kb" 'org-roam-build-cache
+ "Kd" 'org-roam-date
+ "Kf" 'org-roam-tomorrow
+ "Kg" 'org-roam-graph
+ "Ki" 'org-roam-insert
+ "Kl" 'org-roam-open-at-point
+ "Kt" 'org-roam-today
+ "Ky" 'org-roam-yesterday
+
  "gc" 'org-clock-goto
+ "gh" 'org-capture-goto-last-stored
  "gp" 'org-projectile-goto-location-for-project
  "gi" 'jag-open-inbox
  "gg" 'jag-open-gtd
- "e" 'org-clock-modify-effort-estimate
- "f" 'org-resolve-clocks
- "s" 'jag-org-schedule-capture
- "c" 'evil-operator-org-capture
- "C" 'org-capture)
+
+ "Rr" 'evil-operator-remember
+ "Rl" 'remember
+
+ "rr" 'evil-operator-org-capture
+ "rl" 'org-capture-goto-last-stored
+ "rc" 'org-capture)
 
 (use-package jag-funcs-organize
   :commands
@@ -48,7 +75,8 @@
    jag-org-interrupt-capture
    jag-org-journal-capture
    jag-org-clock-gtd
-   jag-org-shedule-capture)
+   jag-org-shedule-capture
+   jag-org-clock-select-task)
   :ensure nil)
 
 (provide 'jag-keys-organize)
