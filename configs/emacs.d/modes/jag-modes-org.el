c648492024bc8e26370982ed9eb8f1b32f793dc0
speyejack <speyejack@gmail.com>
2021-06-24 14:39:04 -0800

    Reindented org keys

--- configs/emacs.d/modes/jag-modes-org.el
+++ configs/emacs.d/modes/jag-modes-org.el
@@ -12,126 +12,127 @@
 (jag-declare-prefix-for-mode 'org-mode "tt" "toggle")
 (jag-declare-prefix-for-mode 'org-mode "td" "delete")
 (jag-define-leader-keys-for-major-mode
-  'org-mode
+ 'org-mode
 
-  "'" 'org-edit-special
-  "y" 'org-copy-special
-  "x" 'org-cut-special
-  "p" 'org-cut-special
-  "c" 'org-columns
-  "C" 'org-capture
-  "d" 'org-deadline
-  "D" 'org-insert-drawer
-  "e" 'org-export-dispatch
-  "f" 'org-set-effort
-  "P" 'org-pomodoro
-  ":" 'org-set-tags-command
+ "'" 'org-edit-special
+ "y" 'org-copy-special
+ "x" 'org-cut-special
+ "p" 'org-cut-special
+ "c" 'org-columns
+ "C" 'org-capture
+ "d" 'org-deadline
+ "D" 'org-insert-drawer
+ "e" 'org-export-dispatch
+ "f" 'org-set-effort
+ "P" 'org-pomodoro
+ ";" 'org-set-tags-command
 
-  "a" 'org-archive-subtree
-  "b" 'org-tree-to-indirect-buffer
-  "A" 'jag-org-archive-done-tasks
-  "l" 'org-open-at-point
-  "T" 'org-show-todo-tree
+ "a" 'org-archive-subtree
+ "b" 'org-tree-to-indirect-buffer
+ "A" 'jag-org-archive-done-tasks
+ "l" 'org-open-at-point
+ "T" 'org-show-todo-tree
 
-  "." 'org-time-stamp
-  "!" 'org-time-stamp-inactive
+ "." 'org-time-stamp
+ "!" 'org-time-stamp-inactive
 
-  ;; headings
-  "hi" 'org-insert-heading-after-current
-  "hI" 'org-insert-heading
-  "hs" 'org-insert-subheading
-  "hu" 'org-update-statistics-cookies
+ ;; headings
+ "hi" 'org-insert-heading-after-current
+ "hI" 'org-insert-heading
+ "hs" 'org-insert-subheading
+ "hu" 'org-update-statistics-cookies
 
-  ;; More cycling options (timestamps, headlines, items, properties)
-  "L" 'org-shiftright
-  "H" 'org-shiftleft
-  "J" 'org-shiftdown
-  "K" 'org-shiftup
+ ;; More cycling options (timestamps, headlines, items, properties)
+ "L" 'org-shiftright
+ "H" 'org-shiftleft
+ "J" 'org-shiftdown
+ "K" 'org-shiftup
 
-  ;; Change between TODO sets
-  "C-S-l" 'org-shiftcontrolright
-  "C-S-h" 'org-shiftcontrolleft
-  "C-S-j" 'org-shiftcontroldown
-  "C-S-k" 'org-shiftcontrolup
+ ;; Change between TODO sets
+ "C-S-l" 'org-shiftcontrolright
+ "C-S-h" 'org-shiftcontrolleft
+ "C-S-j" 'org-shiftcontroldown
+ "C-S-k" 'org-shiftcontrolup
 
-  ;; Subtree editing
-  "Sl" 'org-demote-subtree
-  "Sh" 'org-promote-subtree
-  "Sj" 'org-move-subtree-down
-  "Sk" 'org-move-subtree-up
+ ;; Subtree editing
+ "Sl" 'org-demote-subtree
+ "Sh" 'org-promote-subtree
+ "Sj" 'org-move-subtree-down
+ "Sk" 'org-move-subtree-up
 
-  ;; tables
-  "ta" 'org-table-align
-  "tb" 'org-table-blank-field
-  "tc" 'org-table-convert
-  "tdc" 'org-table-delete-column
-  "tdr" 'org-table-kill-row
-  "te" 'org-table-eval-formula
-  "tE" 'org-table-export
-  "tg" 'orgtbl-ascii-plot
-  "th" 'org-table-previous-field
-  "tH" 'org-table-move-column-left
-  "tic" 'org-table-insert-column
-  "tih" 'org-table-insert-hline
-  "tiH" 'org-table-hline-and-move
-  "tir" 'org-table-insert-row
-  "tI" 'org-table-import
-  "tj" 'org-table-next-row
-  "tJ" 'org-table-move-row-down
-  "tK" 'org-table-move-row-up
-  "tl" 'org-table-next-field
-  "tL" 'org-table-move-column-right
-  "tn" 'org-table-create-or-convert-from-region
-  "tN" 'org-table-create-with-table.el
-  "tr" 'org-table-recalculate
-  "tR" 'org-table-recalculate-buffer-tables
-  "ts" 'org-table-sort-lines
-  "tT" 'org-table-transpose-table-at-point
-  "ttf" 'org-table-toggle-formula-debugger
-  "tto" 'org-table-toggle-coordinate-overlays
-  "tw" 'org-table-wrap-region
-  "t?" 'org-table-field-info
+ ;; tables
+ "ta" 'org-table-align
+ "tb" 'org-table-blank-field
+ "tc" 'org-table-convert
+ "tdc" 'org-table-delete-column
+ "tdr" 'org-table-kill-row
+ "te" 'org-table-eval-formula
+ "tE" 'org-table-export
+ "tg" 'orgtbl-ascii-plot
+ "th" 'org-table-previous-field
+ "tH" 'org-table-move-column-left
+ "tic" 'org-table-insert-column
+ "tih" 'org-table-insert-hline
+ "tiH" 'org-table-hline-and-move
+ "tir" 'org-table-insert-row
+ "tI" 'org-table-import
+ "tj" 'org-table-next-row
+ "tJ" 'org-table-move-row-down
+ "tK" 'org-table-move-row-up
+ "tl" 'org-table-next-field
+ "tL" 'org-table-move-column-right
+ "tn" 'org-table-create-or-convert-from-region
+ "tN" 'org-table-create-with-table.el
+ "tr" 'org-table-recalculate
+ "tR" 'org-table-recalculate-buffer-tables
+ "ts" 'org-table-sort-lines
+ "tT" 'org-table-transpose-table-at-point
+ "ttf" 'org-table-toggle-formula-debugger
+ "tto" 'org-table-toggle-coordinate-overlays
+ "tw" 'org-table-wrap-region
+ "t?" 'org-table-field-info
 
-  ;; Multi-purpose keys
+ ;; Multi-purpose keys
 
-  "m" 'org-ctrl-c-ctrl-c
-  "," 'org-ctrl-c-ctrl-c
-  "*" 'org-ctrl-c-star
-  "RET" 'org-ctrl-c-ret
-  "-" 'org-ctrl-c-minus
-  "^" 'org-sort
-  "/" 'org-sparse-tree
+ "m" 'org-ctrl-c-ctrl-c
+ "," 'org-ctrl-c-ctrl-c
+ "*" 'org-ctrl-c-star
+ "RET" 'org-ctrl-c-ret
+ "-" 'org-ctrl-c-minus
+ "^" 'org-sort
+ "/" 'org-sparse-tree
 
-  "I" 'org-clock-in
-  "n" 'org-narrow-to-subtree
-  "N" 'widen
-  "o" 'org-toggle-inline-images
-  "O" 'org-clock-out
-  "q" 'org-clock-cancel
-  "r" 'jag-org-refile
-  "R" 'jag-org-refile-all
-  "C-r" 'jag-org-refile-inbox
-  "C-S-R" 'jag-org-refile-inbox-all
-  "s" 'org-schedule
-  "u" 'org-latex-preview
-  "U" 'jag-scale-latex
-  "g" 'org-plot/gnuplot
-  "G" 'org-goto
+ "I" 'org-clock-in
+ "n" 'org-narrow-to-subtree
+ "N" 'widen
+ "o" 'org-toggle-inline-images
+ "O" 'org-clock-out
+ "q" 'org-clock-cancel
+ "r" 'jag-org-refile
+ "R" 'jag-org-refile-all
+ "C-r" 'jag-org-refile-inbox
+ "C-S-R" 'jag-org-refile-inbox-all
+ "s" 'org-schedule
+ "u" 'org-latex-preview
+ "U" 'jag-scale-latex
+ "g" 'org-plot/gnuplot
+ "G" 'org-goto
 
-  ;; insertion of common elements
-  "it" 'org-insert-structure-template
-  "iu" 'org-cliplink
-  "ia" 'org-attach
-  "is" 'org-download-screenshot
-  "iy" 'org-download-yank
-  "il" 'org-insert-link
-  "if" 'org-footnote-new
-  "ik" 'org-roam-insert
-  "in" 'org-add-note
-  "iL" 'org-ref-helm-insert-label-link
-  "ir" 'org-ref-helm-insert-ref-link
-  "ic" 'org-ref-helm-insert-cite-link
-  "ip" 'org-set-property)
+ ;; insertion of common elements
+ "it" 'org-insert-structure-template
+ "iT" 'org-transclusion-mode
+ "iu" 'org-cliplink
+ "ia" 'org-attach
+ "is" 'org-download-screenshot
+ "iy" 'org-download-yank
+ "il" 'org-insert-link
+ "if" 'org-footnote-new
+ "ik" 'org-roam-insert
+ "in" 'org-add-note
+ "iL" 'org-ref-helm-insert-label-link
+ "ir" 'org-ref-helm-insert-ref-link
+ "ic" 'org-ref-helm-insert-cite-link
+ "ip" 'org-set-property)
 
 (use-package jag-funcs-org
   :straight nil
@@ -383,7 +384,7 @@
   :config
   (push (org-projectile-project-todo-entry) org-capture-templates)
   (setq org-projectile-projects-file
-		  (expand-file-name "gtd.org" jag-gtd-dir))
+		(expand-file-name "gtd.org" jag-gtd-dir))
   (setq org-projectile-capture-template
 		"* TODO %i%?\n  %a\n"))
 
