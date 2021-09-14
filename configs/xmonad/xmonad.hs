a6f4d38e21912d690e5fb122089b58fcaec47a26
speyejack <speyejack@gmail.com>
2021-09-13 20:15:12 -0700

    Added dunst to startup

--- configs/xmonad/xmonad.hs
+++ configs/xmonad/xmonad.hs
@@ -131,6 +131,7 @@ myStartupHook = do
           spawnOnce "picom --experimental-backends &"
           spawnOnce "unclutter &"
           spawnOnce "setxkbmap -option caps:super"
+          spawnOnce "dunst"
           -- spawnOnce "nm-applet &"
           -- spawnOnce "volumeicon &"
           -- spawnOnce "trayer --edge top --align right --widthtype request --padding 6 --SetDockType true --SetPartialStrut true --expand true --monitor 1 --transparent true --alpha 0 --tint 0x282c34  --height 22 &"
